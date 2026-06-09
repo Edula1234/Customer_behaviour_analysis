f50e1ce6ad27ba08214aff2d134d985cababf881
Pranith <130446457+Edula1234@users.noreply.github.com>
2026-06-09 16:23:47 +0530

    Update business analysis customer_shopping_behaviour.sql

--- business analysis customer_shopping_behaviour.sql
+++ business analysis customer_shopping_behaviour.sql
@@ -54,6 +54,7 @@ from customer
 select customer_segment, count(*) as "Number of Customers"
 from customer_type
 group by customer_segment;
+
 --Q8. What are the top 3 most purchased products within each category?
 with item_counts as (
 select category, item_purchased,
@@ -66,6 +67,7 @@ group by category, item_purchased
 select item_rank, category, item_purchased, total_orders
 from item_counts
 where item_rank <= 3;
+
 --Q9. Are customers who are repeat buyers (more than 5 previous purchases) also likely to subscribe?
 select subscription_status, count(customer_id) as repeat_buyers
 from customer
